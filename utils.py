7f7e3a4b9ac761ed37212bffb375afaeae7e01a7
FelipeZero <felipe.rojas@stud.th-deg.de>
2024-08-13 19:56:16 +0200

    added a return to the check_path and check_file functions to have a way to check during code tests in other classes

--- utils.py
+++ utils.py
@@ -5,12 +5,16 @@ def check_path(folder_path, create=False):
 
     Args:
         path (str): Path to check
+
+    Returns:
+        True if the path exists
     """
     if not os.path.exists(folder_path):
         print(f'Path {folder_path} does not exist.')
         if create:
             os.makedirs(folder_path)
             print(f'Path {folder_path} created successfully.')
+            return True
         else:
             raise FileNotFoundError
 
@@ -23,9 +27,14 @@ def check_file(path_to_file):
 
     Args:
         path_to_file (str): Path to the file
+
+    Returns:
+        True if the file exists
     """
     if not os.path.exists(path_to_file):
         print(f'File {path_to_file} does not exist.')
         raise FileNotFoundError
     else:
+
         print(f'File {path_to_file} exists.')
+        return True
