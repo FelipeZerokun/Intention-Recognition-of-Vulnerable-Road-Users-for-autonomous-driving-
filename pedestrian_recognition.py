a011aac463b0445072ed29912d143825963092fa
FelipeZero <felipe.rojas@stud.th-deg.de>
2024-11-17 07:06:19 +0100

    added the base depth unit scale for the camera series 400 as a string called _DEPTH_UNIT_SCALE

--- pedestrian_recognition.py
+++ pedestrian_recognition.py
@@ -7,6 +7,8 @@ import os
 
 #load YOLOv5 model
 class PedestrianRecognition:
+
+    _DEPTH_UNIT_SCALE = 0.001
     def __init__(self, data_directory: Path):
         """Class for pedestrian recognition using YOLOv5 model
         """
@@ -74,15 +76,16 @@ class PedestrianRecognition:
 
             for person in persons:
                 x1, y1, x2, y2, confidence, class_id = person
-                if confidence < 0.6:
+                if confidence < 0.5:
                     continue
                 cv2.rectangle(color_frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
                 roi = depth_frame[int(y1):int(y2), int(x1):int(x2)]
                 mean_depth = roi.mean()
+                pedestrian_distance = mean_depth * self._DEPTH_UNIT_SCALE
                 cv2.putText(color_frame, f'Person {confidence:.2f}', (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                cv2.putText(color_frame, f'Depth: {mean_depth:.2f} m', (int(x1), int(y1 - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                cv2.putText(color_frame, f'Depth: {pedestrian_distance:.2f} m', (int(x1), int(y1 - 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
             cv2.imshow('Pedestrian detection', color_frame)
-            if cv2.waitKey(2000) & 0xFF == ord('q'):
+            if cv2.waitKey(200) & 0xFF == ord('q'):
                 break
 
         cv2.destroyAllWindows()
