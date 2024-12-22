656b8ba2c0c6d4e0b6ae119f54026b7769b65126
FelipeZerokun <epilefrojasleon@gmail.com>
2024-12-22 16:50:57 +0100

    New class to "modify" the action recognition classes into a Pedestrian intent prediction classes

--- datasets/action_intent_class_creator.py
+++ datasets/action_intent_class_creator.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 import cv2
-from project_utils.project_utils import check_path
+from project_utils.project_utils import check_path, check_os_windows
 class IntentPredictionDataset:
     def __init__(self, actions_dataset_dir: str, frames_data_dir: str, output_dir: str):
 
@@ -56,11 +56,15 @@ class IntentPredictionDataset:
     def annotate_intention(self):
         for action_number_dir in self._action_number_dirs():
             files_in_action_folder = os.listdir(action_number_dir)
-            print("annotating for action: ", action_number_dir)
+            action = action_number_dir.split('/')[-2]
+            pedestrian = action_number_dir.split('/')[-1]
+            print(f"annotating for action {action} for pedestrian {pedestrian}")
             # Perform labeling here
             for file in files_in_action_folder:
+                if file.endswith('.csv'):
+                    frame_data = pd.read_csv(os.path.join(action_number_dir, file))
+                    # self._watch_frames(frame_data)
                 if file.endswith('.png'):
-                    intent = 'unknown'
                     frame = cv2.imread(os.path.join(action_number_dir, file))
                     cv2.imshow('Frame viewer', frame)
                     key = cv2.waitKey(0) & 0xFF
@@ -69,11 +73,93 @@ class IntentPredictionDataset:
                     if key == ord('d'):
                         continue
 
-                if intent == 'unknown':
-                    intent = input("Enter the intention of the pedestrian: ")
+                    if key == ord('w'):
+                        intent = 'walk'
+
+            self._watch_frames(frame_data)
+            intent_from_user = input("Did the pedestrian intents to change their action? (y/n): ").lower()
+            if intent_from_user == 'y':
+                if action == 'walking':
+                    intent_label = 'stop_walking'
+                    intent = 1
+                elif action == 'standing_still':
+                    intent_label = 'start_walking'
+                    intent = 1
+
+            elif intent_from_user == 'n':
+                if action == 'walking':
+                    intent_label = 'continue_walking'
+                    intent = 0
+                elif action == 'standing_still':
+                    intent_label = 'continue_standing_still'
+                    intent = 0
+
+            # Add intent_label and intent as new colums to the frame_data
+            frame_data['intent_label'] = intent_label
+            frame_data['intent'] = intent
+
+            # Save the new frame_data
+            frame_data.to_csv(os.path.join(action_number_dir, 'intention_data.csv'), index=False)
+
+    def _watch_frames(self, frame_data):
+        timestamps = list(frame_data['timestamp'])
+        first_timestamp = timestamps[0]
+        last_timestamp = timestamps[-1]
+
+        frames = self._look_for_correct_video(first_timestamp, last_timestamp)
+        color_frames = frames['rgb_frame'].tolist()
+        depth_frames = frames['depth_frame'].tolist()
+        timestamps = frames['timestamp'].tolist()
+
+        index = 0
+        while True:
+            frame_path = color_frames[index]
+            depth_path = depth_frames[index]
+            frame_path = check_os_windows(frame_path)
+            depth_path = check_os_windows(depth_path)
+
+            frame = cv2.imread(frame_path)
+            depth_map = cv2.imread(depth_path, cv2.IMREAD_UNCHANGED)
+            # if values from the depth map are greater than 5000, make them 0
 
 
+            cv2.imshow('Frame viewer', frame)
+            key = cv2.waitKey(0) & 0xFF
+            if key == 27:  # ESC key
+                break
 
+            if key == ord('a'):
+                if index == 0:
+                    print('Already at the first frame.')
+                    continue
+                else:
+                    index -= 1
+                    continue
+
+            if key == ord('d'):
+                if index == len(color_frames) - 1:
+                    print('Already at the last frame.')
+                    continue
+                else:
+                    index += 1
+                    continue
+
+    def _look_for_correct_video(self, first_frame, last_frame):
+        video_01_path = os.path.join(self.frames_data_dir, 'video_01/navigation_data.csv')
+        video_02_path = os.path.join(self.frames_data_dir, 'video_02/navigation_data.csv')
+        video_03_path = os.path.join(self.frames_data_dir, 'video_03/navigation_data.csv')
+        video_04_path = os.path.join(self.frames_data_dir, 'video_04/navigation_data.csv')
+
+        videos_path = [video_01_path, video_02_path, video_03_path, video_04_path]
+        for video_path in videos_path:
+            if not os.path.exists(video_path):
+                raise FileNotFoundError(f"Video {video_path} not found")
+
+            frames_data = pd.read_csv(video_path)
+            timestamps = list(frames_data['timestamp'])
+            if first_frame in timestamps and last_frame in timestamps:
+                # Return all the rows that are between the first and last frame
+                return frames_data[(frames_data['timestamp'] >= first_frame)]
 
 def main():
     frames_data_dir = '/media/felipezero/T7 Shield/DATA/thesis/Videos/'
